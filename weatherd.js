57edc0d57920be45a0fa54030bbe2894fc1bcf2a
ishu-23 <ishwarikadav123@gamil.com>
2021-03-28 21:58:31 +0530

    second commet

--- weatherd.js
+++ weatherd.js
@@ -1,15 +1,13 @@
-var searchinput=document.getElementById("search-input");
-const searchbutton=document.getElementById("search-button");
-const apikey="b807d9abba407f927991b9548a707920";
-const url="https://api.openweathermap.org/data/2.5/weather?q={searchinput}&appid={apikey}";
-async function getapi(url)
-{
+async function hello()
+{ const key="b807d9abba407f927991b9548a707920";
+  var searchinput=document.getElementById("search-input").value;
+  const url='https://api.openweathermap.org/data/2.5/weather?q=Bhoom&appid=b807d9abba407f927991b9548a707920';
+  console.log(searchinput);
   const response =await fetch(url);
   var data=await response.json();
-  alert(data);
-  alert('hi');
+
+  console.log(data);
 }
-getapi(url);
 
 
 
